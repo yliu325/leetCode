45c1b11322ba9409788713bd586875e82fcbebb5
Liu Yang <liuyang910612@gmail.com>
2020-03-28 23:55:47 +0800

    945	使数组唯一的最小增量

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Yang on 2020/3/22.
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        MinIncrementForUnique minIncrementForUnique = new MinIncrementForUnique();
+        System.out.println(minIncrementForUnique.minIncrementForUnique(new int[]{2,2,2,1}));
+    }
+}
